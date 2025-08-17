d09772714b90c128d66211e5c7a80a5c2869c88a
Mohammad Faseeh <145072594+MohammadFaseeh@users.noreply.github.com>
2025-08-17 15:39:11 +0500

    8_maths.js

--- 8_maths.js
+++ 8_maths.js
@@ -1,6 +1,6 @@
-//* =========================================
+//* ==========================
 //* Math Object in JavaScript
-//* =========================================
+//* ==========================
 
 //* Math: The Math namespace object contains static properties and methods for mathematical constants and functions.
 //* Math works with the Number type. It doesn't work with BigInt.
@@ -8,37 +8,37 @@
 //? 1: Constants:
 //* - Math.PI: Represents the mathematical constant Pi (π).
 // const piValue = Math.PI;
-// console.log(piValue);
+// console.log(piValue); // 3.141592653589793
 
 //* =========================================
-//* 2. Basic Operations:
+//* 1. Basic Operations:
 //* =========================================
 
 //? Math.abs(): The Math.abs() static method returns the absolute value of a number.
 // or in simple, how far the number is from 0. It will be always positive
 
-// console.log(Math.abs(5));
-// console.log(Math.abs(-115));
+// console.log(Math.abs(5)); // 5
+// console.log(Math.abs(-115)); // 115
 
 //? Math.round(x): Rounds a number to the nearest integer.
 // const roundedValue = Math.round(3.7);
-// console.log(roundedValue);
+// console.log(roundedValue); //4
 
 //? Math.ceil(x): Returns the value of x rounded up to its nearest integer:
 // const ceilValue = Math.ceil(3.7);
-// console.log(ceilValue);
+// console.log(ceilValue); //4
 
 //? Math.floor(x): Returns the value of x rounded down to its nearest integer.
 // const floorValue = Math.floor(3.7);
-// console.log(floorValue);
+// console.log(floorValue); //3
 
 //? Math.trunc(x): Returns the integer part of x:
-// const truncValue = Math.trunc(3.7);
-// console.log(truncValue);
+// const truncValue = Math.trunc(15.7);
+// console.log(truncValue); //15
 
 //todo Math.trunc() and Math.sign() were added to JavaScript 2015 - ES6.
-// const truncValue = Math.trunc(-3.7);
-// const floorValue = Math.floor(-3.1);
+// const truncValue = Math.trunc(-3.7); //-3
+// const floorValue = Math.floor(-3.1); //-4
 // console.log(truncValue);
 // console.log(floorValue);
 
@@ -49,24 +49,24 @@
 // ceil always rounds up.
 
 //* =========================================
-//* 3. Exponential and Logarithmic Functions:
+//* 2. Exponential and Logarithmic Functions:
 //* =========================================
 
 //? Math.pow(x, y): Returns the value of x to the power of y.
-// console.log(Math.pow(2, 5));
-// console.log(2 ** 5);
+// console.log(Math.pow(2, 5)); // used but not recommended
+// console.log(2 ** 5); // use this its new way
 
 //? Math.sqrt(): Math.sqrt(x) returns the square root of x:
 // let squareRoot = Math.sqrt(25);
-// console.log(squareRoot);
+// console.log(squareRoot); // 5
 
 //? Math.log(x) returns the natural logarithm of x.
-// let logResult = Math.log(1);
-// let logResult = Math.log(2);
+// let logResult = Math.log(1); // 0
+// let logResult = Math.log(2); // 0.6931471805599453
 // console.log(logResult);
 
 //? Math.log2(x) returns the base 2 logarithm of x.
-// let logResult = Math.log2(8);
+// let logResult = Math.log2(8); //3
 // console.log(logResult);
 
 //? Math.max(a, b, ...)	Returns the largest value	
@@ -74,6 +74,29 @@
 
 //? Math.min(a, b, ...)	Returns the smallest value	
 // Math.min(2, 5, 1) → 1
+
+//* =================================
+//* Now Numbers Object in JavaScript
+//* =================================
+// 1. toFixed() What it does: Fixes the number of digits after the decimal point.
+// let num = 12.3456;
+// console.log(num.toFixed(2)); // "12.35"
+// console.log(num.toFixed(0)); // "12"
+
+// 2. toString() → make it a string (or binary/hex).
+// let num = 255;
+// console.log(num.toString());   // "255"
+
+// 3. toPrecision() What it does: Sets the total number of significant digits (from start to end of number, not just decimals).
+// let num = 12.3456;
+// console.log(num.toPrecision(2)); // "12"
+// console.log(num.toPrecision(4)); // "12.35"
+
+// 4. toLocaleString() Converts number into a local format (with commas, currency, etc.), based on your region.
+// let num = 1234567.89;
+// console.log(num.toLocaleString()); // "1,234,567.89" (US)
+// console.log(num.toLocaleString('de-DE')); // "1.234.567,89" (Germany)
+
 
 //* =========================================
 //* Interview Question
